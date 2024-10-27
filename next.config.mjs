a8280d9a83b84ced531ca4b91cd79ad5a7534c02
Walker Selby <git@walkerselby.com>
2024-10-27 16:09:38 -0700

    Add module.exports standalone
    Signed-off-by: Walker Selby <git@walkerselby.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'standalone',
+};
 
 export default nextConfig;
 
-module.exports = {
-    output: 'standalone',
-}
