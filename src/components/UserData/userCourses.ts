9a0c4168d8ba59e096acd744dbfde69b20c1ae34
Kayleigh Vu <kayleighpvu@gmail.com>
2024-10-29 16:34:40 -0700

    Update userCourses.ts

--- src/components/UserData/userCourses.ts
+++ src/components/UserData/userCourses.ts
@@ -7,11 +7,11 @@ export const userCourses = [
   {
     season: "Winter",
     year: "2021",
-    courses: [{ name: "Operating Systems", id: "CSEN 177", unit: "4" }],
+    courses: [{ name: "Programming Languages", id: "CSEN 171", unit: "4" }],
   },
   {
     season: "Spring",
     year: "2021",
-    courses: [{ name: "Operating Systems", id: "CSEN 177", unit: "4" }],
-  },
+    courses: [{ name: "Software Engineering", id: "CSEN 174", unit: "4" }],
+  }
 ];
