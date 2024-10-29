c76ca0bd542ecb80fb03b2eaaa16d57f33638137
RaphaLK <77198909+RaphaLK@users.noreply.github.com>
2024-10-29 12:35:34 -0700

    fixed page.tsx

--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -11,7 +11,6 @@ import {
 } from "@/components/ui/card";
 
 import NavBar from "@/components/navigation/NavBar";
-import React from "react";
 
 const csenLinks = [
   {
@@ -62,107 +61,110 @@ const contactAdvisorInfo = {
 const peerAdvisorInfo = {
   name: "Jane Smith",
   email: "janesmith@example.com",
-
+};
 
 const ToolsAndUsefulLinks = () => {
   return (
-    <div className="flex flex-col font-sans min-h-screen">
-      <div className="grid grid-cols-4 gap-4 p-4 flex-grow">
-        <Card className="col-span-4">
-          <CardHeader>
-            <CardTitle>Contact Advisors</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="grid grid-cols-5 gap-2 text-sm">
-              <div className="flex flex-col">
-                <CardDescription>Advisor Name</CardDescription>
-                <CardDescription>{contactAdvisorInfo.name}</CardDescription>
+    <div className="flex flex-col min-h-screen">
+      <NavBar isLoggedIn={true} selectedPage={"Tools"}/>
+      <div className="font-sans">
+        <div className="grid grid-cols-4 gap-4 p-4 flex-grow">
+          <Card className="col-span-4">
+            <CardHeader>
+              <CardTitle>Contact Advisors</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="grid grid-cols-5 gap-2 text-sm">
+                <div className="flex flex-col">
+                  <CardDescription>Advisor Name</CardDescription>
+                  <CardDescription>{contactAdvisorInfo.name}</CardDescription>
+                </div>
+                <div className="flex flex-col">
+                  <CardDescription>Phone Number</CardDescription>
+                  <CardDescription>{contactAdvisorInfo.number}</CardDescription>
+                </div>
+                <div className="flex flex-col">
+                  <CardDescription>Email</CardDescription>
+                  <CardDescription>{contactAdvisorInfo.email}</CardDescription>
+                </div>
+                <div className="flex flex-col">
+                  <CardDescription>Office Location</CardDescription>
+                  <CardDescription>
+                    {contactAdvisorInfo.officeLocation}
+                  </CardDescription>
+                </div>
+                <div className="flex flex-col">
+                  <CardDescription>Office Hours</CardDescription>
+                  <CardDescription>
+                    {contactAdvisorInfo.officeHours}
+                  </CardDescription>
+                </div>
               </div>
-              <div className="flex flex-col">
-                <CardDescription>Phone Number</CardDescription>
-                <CardDescription>{contactAdvisorInfo.number}</CardDescription>
-              </div>
-              <div className="flex flex-col">
-                <CardDescription>Email</CardDescription>
-                <CardDescription>{contactAdvisorInfo.email}</CardDescription>
-              </div>
-              <div className="flex flex-col">
-                <CardDescription>Office Location</CardDescription>
-                <CardDescription>
-                  {contactAdvisorInfo.officeLocation}
-                </CardDescription>
-              </div>
-              <div className="flex flex-col">
-                <CardDescription>Office Hours</CardDescription>
-                <CardDescription>
-                  {contactAdvisorInfo.officeHours}
-                </CardDescription>
-              </div>
-            </div>
-          </CardContent>
-        </Card>
+            </CardContent>
+          </Card>
 
-        <Card className="col-span-2">
-          <CardHeader>
-            <CardTitle>Community Advice</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <CardDescription>Peer Advisor A says: ...</CardDescription>
-          </CardContent>
-        </Card>
+          <Card className="col-span-2">
+            <CardHeader>
+              <CardTitle>Community Advice</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <CardDescription>Peer Advisor A says: ...</CardDescription>
+            </CardContent>
+          </Card>
 
-        <Card className="col-span-2">
-          <CardHeader>
-            <CardTitle>Peer Advisors</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <CardDescription>{peerAdvisorInfo.name}</CardDescription>
-            <CardDescription>{peerAdvisorInfo.email}</CardDescription>
-          </CardContent>
-        </Card>
+          <Card className="col-span-2">
+            <CardHeader>
+              <CardTitle>Peer Advisors</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <CardDescription>{peerAdvisorInfo.name}</CardDescription>
+              <CardDescription>{peerAdvisorInfo.email}</CardDescription>
+            </CardContent>
+          </Card>
 
-        <Card className="col-span-2">
-          <CardHeader>
-            <CardTitle>Useful Links for CSEN</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <ul className="list-disc list-inside text-sm">
-              {csenLinks.map((link, index) => (
-                <li key={index} className="hover:text-blue-500">
-                  <a
-                    href={link.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-600 underline"
-                  >
-                    {link.name}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </CardContent>
-        </Card>
-        <Card className="col-span-2">
-          <CardHeader>
-            <CardTitle>Office of the Registrar Links</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <ul className="list-disc list-inside text-sm">
-              {officeOfRegistrarLinks.map((link, index) => (
-                <li key={index} className="hover:text-blue-500">
-                  <a
-                    href={link.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-600 underline"
-                  >
-                    {link.name}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </CardContent>
-        </Card>
+          <Card className="col-span-2">
+            <CardHeader>
+              <CardTitle>Useful Links for CSEN</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <ul className="list-disc list-inside text-sm">
+                {csenLinks.map((link, index) => (
+                  <li key={index} className="hover:text-blue-500">
+                    <a
+                      href={link.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-blue-600 underline"
+                    >
+                      {link.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </CardContent>
+          </Card>
+          <Card className="col-span-2">
+            <CardHeader>
+              <CardTitle>Office of the Registrar Links</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <ul className="list-disc list-inside text-sm">
+                {officeOfRegistrarLinks.map((link, index) => (
+                  <li key={index} className="hover:text-blue-500">
+                    <a
+                      href={link.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-blue-600 underline"
+                    >
+                      {link.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </div>
   );
