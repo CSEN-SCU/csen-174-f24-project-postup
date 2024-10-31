d49b1e7ef1bcc4449835f405299ada3f69bb0027
Walker Selby <git@walkerselby.com>
2024-10-31 10:46:41 -0700

    Add fake error to test Sentry.
    Signed-off-by: Walker Selby <git@walkerselby.com>

--- src/components/AddClass/AddClass.tsx
+++ src/components/AddClass/AddClass.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { AddClassProp } from "@/app/utils/interfaces";
+import * as Sentry from "@sentry/browser"
 
 const AddClass: React.FC<AddClassProp> = ({
   setAddingClass,
@@ -13,6 +14,7 @@ const AddClass: React.FC<AddClassProp> = ({
         onClick={() => {
           setAddingClass(true);
           setSelectedQuarter([season, year]);
+          Sentry.captureMessage("Add Class Button Clicked; Test","fatal");
         }}
       >
         <div className="rounded-sm transition ease-in-out delay-10 bg-slate-500 shadow-md hover:bg-slate-400 active:bg-slate-500 text-center p-2 mt-2">
