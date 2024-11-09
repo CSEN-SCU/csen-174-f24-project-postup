bf21db065ff9ed0c6bee4fc1a9f45da47465de14
Raphael Kusuma <77198909+RaphaLK@users.noreply.github.com>
2024-11-08 19:25:59 -0800

    updated max-width to avoid screen filling

--- src/components/DraggableCards/DragDropCourses.tsx
+++ src/components/DraggableCards/DragDropCourses.tsx
@@ -86,7 +86,7 @@ const DragDropCourses: React.FC<DragDropCardProps> = ({
 
   return (
     <DndContext onDragEnd={handleDragEnd}>
-      <div className="flex flex-col gap-4 min-w-1/2">
+      <div className="flex flex-col gap-4 min-w-1/2 max-w-1/2 overflow-scroll">
         {/* Render Droppable Quarters */}
         <div className="grid grid-cols-3 gap-4 ">
           {userPlan.map(({ season, year }) => (
