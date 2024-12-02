d25e84cd02b968812ce2930801fdbd900d67df58
Raphael Kusuma <77198909+RaphaLK@users.noreply.github.com>
2024-12-02 10:44:53 -0800

    updated cores to put in "pretty names" instead

--- src/DegreeRequirements/Majors/CORE.ts
+++ src/DegreeRequirements/Majors/CORE.ts
@@ -4,20 +4,54 @@ export const core_reqs_ENGR = [
   {
     requirement: "Core Explorations",
     courses: [
-      { courseId: "Core Foundations :: Critical Thinking and Writing 1" },
-      { courseId: "Core Foundations :: Critical Thinking and Writing 2" },
-      { courseId: "Core Foundations :: Culture and Ideas 1" },
-      { courseId: "Core Foundations :: Culture and Ideas 2" },
-      { courseId: "Core Foundations :: Culture and Ideas 3" },
-      { courseId: "Core Foundations :: Religion, Theology and Culture 1"},
-      { courseId: "Core Foundations :: Religion, Theology and Culture 2" },
-      { courseId: "Core Explorations :: Religion, Theology and Culture 3" },
-      { courseId: "Core Explorations :: Ethics" },
-      { courseId: "Core Explorations :: Diversity" },
-      { courseId: "Core Explorations :: Civic Engagement" },
-      { courseId: "Core Explorations :: Natural Sciences" },
-      { courseId: "Core Explorations :: Arts" },
-      { courseId: "Core Explorations :: Science, Technology, and Society" },
+      {
+        prettyName: "CTW 1",
+        courseId: "Core Foundations :: Critical Thinking and Writing 1",
+      },
+      {
+        prettyName: "CTW 2",
+        courseId: "Core Foundations :: Critical Thinking and Writing 2",
+      },
+      {
+        prettyName: "C&I 1",
+        courseId: "Core Foundations :: Culture and Ideas 1",
+      },
+      {
+        prettyName: "C&I 2",
+        courseId: "Core Foundations :: Culture and Ideas 2",
+      },
+      {
+        prettyName: "C&I 3",
+        courseId: "Core Foundations :: Culture and Ideas 3",
+      },
+      {
+        prettyName: "RTC 1",
+        courseId: "Core Foundations :: Religion, Theology and Culture 1",
+      },
+      { 
+        prettyName: "RTC 2",
+        courseId: "Core Foundations :: Religion, Theology and Culture 2" },
+      { 
+        prettyName: "RTC 3",
+        courseId: "Core Explorations :: Religion, Theology and Culture 3" },
+      { 
+        prettyName: "Ethics",
+        courseId: "Core Explorations :: Ethics" },
+      { 
+        prettyName: "Diversity",
+        courseId: "Core Explorations :: Diversity: US Perspectives" },
+      { 
+        prettyName: "Civic Engagement",
+        courseId: "Core Explorations :: Civic Engagement" },
+      { 
+        prettyName: "Natural Sciences",
+        courseId: "Core Explorations :: Natural Sciences" },
+      { 
+        prettyName: "Arts",
+        courseId: "Core Explorations :: Arts" },
+      { 
+        prettyName: "Science, Technology, and Society",
+        courseId: "Core Explorations :: Science, Technology, and Society" },
     ],
   },
   {
